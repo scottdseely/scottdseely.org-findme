170aa23685dffcb5a49e9a0db56e2a50f9f320f6
Scott Seely <scott.seely@sfsoma.com>
2023-02-24 17:25:26 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ st.info('Developer Advocate, Content Creator and ex-Professor with an interest i
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('youtube', 'https://youtube.com/scottdseely', 'Data Professor YouTube channel', icon_size)
+st_button('youtube', 'https://youtube.com/scottdseely', 'Coding Professor YouTube channel', icon_size)
+st_button('medium', 'https://medium.com/@ScottDSeely/lists', 'Read my Blogs', icon_size)
+st_button('twitter', 'https://twitter.com/scottdseely/', 'Follow me on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/scottdseely/', 'Follow me on LinkedIn', icon_size)
+st_button('newsletter', 'https://sendfox.com/scottdseely/', 'Sign up for my Newsletter', icon_size)
+st_button('cup', 'https://www.buymeacoffee.com/scottdseely/', 'Buy me a Coffee', icon_size)
